1d4cdc8ac945610de4eaeff71fbecc73888da9db
James Burke <k1811535@kingston.ac.uk>
2021-02-25 01:49:54 +0000

    Interactable: trigger enter event 1
    script handles trigger enter event, if its the player then the trigger sets off. nothing specific happens yet

---
+++ Assets/My Scripts/InteractionHandle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHandle : MonoBehaviour
+{
+
+    private CircleCollider2D TriggerBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggerBounds = GetComponent<CircleCollider2D>();
+        if (!TriggerBounds)
+        {
+            Debug.Log(gameObject.name + " has no trigger attached (Missing CircleCollider2D)");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger of '" + gameObject.name + "'");
+        }
+    }
+}
