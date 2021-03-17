782278825a7163a8217d89921578c4907bd06e10
James Burke <k1811535@kingston.ac.uk>
2021-03-17 00:16:53 +0000

    Fade In/Out Implementation: Found Sally
    Works for transition from when the player finds Sally. No specific scene is loaded yet

---
+++ Assets/My Scripts/SceneChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator Animator;
+
+    public int levelToLoad = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToScene(int sceneIndex)
+    {
+        Animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
