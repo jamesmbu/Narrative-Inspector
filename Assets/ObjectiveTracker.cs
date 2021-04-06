3a3c60ce85613ec31963319a4129e277027d46ee
James Burke <k1811535@kingston.ac.uk>
2021-04-06 20:29:38 +0100

    Objective Completion Based Scene Transition - Crime Scene > Office

--- Assets/ObjectiveTracker.cs
+++ Assets/ObjectiveTracker.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectiveTracker : MonoBehaviour
 {
     public int TalliedObjectives = 0;
     public int TotalObjectives = 0;
 
+    private SceneChanger SceneChanger;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneChanger = GetComponent<SceneChanger>();
     }
 
     public void TallyObjective()
@@ -19,6 +21,7 @@ public class ObjectiveTracker : MonoBehaviour
         if (AllObjectivesMet(TalliedObjectives))
         {
             Debug.Log("Objectives complete");
+            SceneChanger.FadeToScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
